5a84074b3a3db8f56d4e7a71179fd39249f669cb
tuan2k2 <tuana156@gmail.com>
2022-08-30 19:21:26 +0700

    push l2

--- app/src/main/java/com/example/giaodiendangnhap/MainActivity.java
+++ app/src/main/java/com/example/giaodiendangnhap/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.giaodiendangnhap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trangdangnhap);
+        btnLogin = (Button) findViewById(R.id.button);
+         btnLogin.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent =  new Intent(MainActivity.this,TrangcanhanMainActivity.class);
+                 startActivity(intent);
+             }
+         });
     }
 }
