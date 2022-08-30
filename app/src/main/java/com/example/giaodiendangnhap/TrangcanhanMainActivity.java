078acaed8091dcde4cc27b0f46d31b55ac532bc1
tuan2k2 <tuana156@gmail.com>
2022-08-30 17:55:23 +0700

    push

---
+++ app/src/main/java/com/example/giaodiendangnhap/TrangcanhanMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.giaodiendangnhap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangcanhanMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trangcanhan_main);
+    }
+}
